085057f3793551fa13dfad39b45da1010cc4149c
HeliXZz <helixzz@home-gateway.168.10.140>
2017-11-18 23:58:47 +0800

    modified:   install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -3,6 +3,9 @@
 # Reminder
 read -p "You're installing [ ss-server ]. Confirm or hit Ctrl-C to exit."
 
+# Init
+dt=`date +%s`
+
 # Install prerequisites
 sudo apt-get update
 sudo apt-get -y -qq install libev4 libsodium18 libmbedcrypto0 libc-ares2
@@ -18,6 +21,9 @@ chmod a+x assets/ss-server/etc/init.d/shadowsocks-libev
 cp -rf assets/ss-server/etc/init.d/shadowsocks-libev /etc/init.d/
 cp -rf assets/ss-server/etc/default/shadowsocks-libev /etc/default/
 mkdir -p /etc/shadowsocks-libev
+if [ -f /etc/shadowsocks-libev/config.json ]; then
+    mv /etc/shadowsocks-libev/config.json /etc/shadowsocks-libev/config.json.backup_$dt
+fi
 cp -rf assets/ss-server/etc/shadowsocks-libev/config.json /etc/shadowsocks-libev/
 update-rc.d shadowsocks-libev defaults 99
 echo "Please fill up ports and passwords by yourself."
